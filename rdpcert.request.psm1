45a93766398a15779a5b4f3fbe76f74833045da5
petaljoe_ <102842542+Spring2022abcjk@users.noreply.github.com>
2026-02-22 19:38:53 +0800

    Add PowerShell requirements and error handling

--- rdpcert.request.psm1
+++ rdpcert.request.psm1
@@ -1,3 +1,9 @@
+Requires -Version 7.0
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 function Request-RdpCert {
     param(
         [Parameter(Mandatory)]
